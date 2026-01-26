cc5a2d6cf71aa01671e7d60ecb453688a32c9293
Henry <coolhwkgv@gmail.com>
2026-01-26 14:42:09 -0800

    adjusted use of pow in secret_key, works with large hex values

--- task1.py
+++ task1.py
@@ -28,13 +28,14 @@ class User:
             raise ValueError("Other user's public key not received yet.")
 
         else:
-            power = pow(self.other_user_pub_key, self.priv_key)
-            return power % q
+            return pow(self.other_user_pub_key, self.priv_key, q)
 
 # assume both A and B get same IV
 
 q = 37
 alpha = 5
+# q = 0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371
+# alpha = 0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5
 
 bob = User('Bob')
 alice = User('Alice')
@@ -48,17 +49,17 @@ alice.generate_pub_key(alpha, q)
 bob.receive_pub_key(alice.pub_key)
 alice.receive_pub_key(bob.pub_key)
 
-print(f"Bob's Private Key: {bob.priv_key}")
-print(f"Bob's Public Key: {bob.pub_key}")
+print(f"Bob's Private Key: {hex(bob.priv_key)}")
+print(f"Bob's Public Key: {hex(bob.pub_key)}")
 
-print(f"Alice's Private Key: {alice.priv_key}")
-print(f"Alice's Public Key: {alice.pub_key}")
+print(f"Alice's Private Key: {hex(alice.priv_key)}")
+print(f"Alice's Public Key: {hex(alice.pub_key)}")
 
-print(f"Bob received Alice's Public Key: {bob.other_user_pub_key}")
-print(f"Alice received Bob's Public Key: {alice.other_user_pub_key}")
+print(f"Bob received Alice's Public Key: {hex(bob.other_user_pub_key)}")
+print(f"Alice received Bob's Public Key: {hex(alice.other_user_pub_key)}")
 
 bob_secret = bob.secret_key(q)
 alice_secret = alice.secret_key(q)
 
-print(f"Bob's Secret Key: {bob_secret}")
-print(f"Alice's Secret Key: {alice_secret}")
+print(f"Bob's Secret Key: {hex(bob_secret)}")
+print(f"Alice's Secret Key: {hex(alice_secret)}")
